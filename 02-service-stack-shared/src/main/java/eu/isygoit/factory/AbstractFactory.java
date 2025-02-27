63d6eeec3ac4116ae3cae8646ba355acccbe5111
S.Mbarki <s.mbarki@novobit.eu>
2025-02-27 12:30:14 +0100

    rename CodifiableService

--- 02-service-stack-shared/src/main/java/eu/isygoit/factory/AbstractFactory.java
+++ 02-service-stack-shared/src/main/java/eu/isygoit/factory/AbstractFactory.java
@@ -1,19 +1,43 @@
 package eu.isygoit.factory;
 
+import java.lang.reflect.ParameterizedType;
+
 /**
  * The type Abstract factory.
  *
  * @param <T> the type parameter
  */
 public abstract class AbstractFactory<T> implements Factory<T> {
-
     private T instance;
+    protected final Class<T> type;
+
+    @SuppressWarnings("unchecked")
+    public AbstractFactory() {
+        // Extract generic type parameter once during construction
+        type = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass())
+                .getActualTypeArguments()[0];
+    }
 
     @Override
     public T instance() {
-        if (this.instance == null) {
-            this.instance = newInstance();
+        // Double-checked locking pattern for thread safety and performance
+        if (instance == null) {
+            synchronized (this) {
+                if (instance == null) {
+                    instance = newInstance();
+                }
+            }
         }
-        return this.instance;
+        return instance;
+    }
+
+    @Override
+    public T newInstance() {
+        try {
+            // Direct call to newInstance() is simpler in most cases than using getDeclaredConstructor()
+            return type.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot instantiate " + type.getName(), e);
+        }
     }
 }
