f6c94df35b45c7914f02d205179a7baf1203bcd5
SMB <sami.mbarki@gmail.com>
2026-08-19 12:28:03 +0100

    refactoring

--- 09-service-stack-storage/src/main/java/eu/isygoit/s3/object/MetaData.java
+++ 09-service-stack-storage/src/main/java/eu/isygoit/s3/object/MetaData.java
@@ -1,7 +1,9 @@
 package eu.isygoit.s3.object;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
@@ -14,6 +16,8 @@ import java.util.Map;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MetaData {
     private String objectName;
     private String bucketName;
