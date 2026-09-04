129cebfff06aace5f2d64b12d987f109e354bf54
SMB <sami.mbarki@gmail.com>
2026-09-04 01:29:17 +0100

    refactoring

--- 02-service-stack-shared/src/main/java/eu/isygoit/dto/common/TimeSlot.java
+++ 02-service-stack-shared/src/main/java/eu/isygoit/dto/common/TimeSlot.java
@@ -1,14 +1,13 @@
 package eu.isygoit.dto.common;
 
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public interface TimeSlot {
 
     public DayOfWeek getDayOfWeek();
-    public LocalDateTime getStartTime();
-    public LocalDateTime getEndTime() ;
+    public LocalTime getStartTime();
+    public LocalTime getEndTime() ;
 
     public String getDescription();
 }
