afdae168e65cc7166365d230262f1f25c329e511
SMB <sami.mbarki@gmail.com>
2026-09-04 08:45:32 +0100

    refactoring

---
+++ 02-service-stack-shared/src/main/java/eu/isygoit/dto/common/CalendarTimeSlot.java
@@ -0,0 +1,14 @@
+package eu.isygoit.dto.common;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public interface CalendarTimeSlot {
+
+    public DayOfWeek getDayOfWeek();
+    public LocalDateTime getStartTime();
+    public LocalDateTime getEndTime() ;
+
+    public String getDescription();
+}
