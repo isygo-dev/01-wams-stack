e2403420c63a54745a5ccb00a9a31555942e669c
SMB <sami.mbarki@gmail.com>
2026-09-15 14:39:20 +0100

    add category info for linked files

--- 02-service-stack-shared/src/main/java/eu/isygoit/mapper/EntityMinFullMapper.java
+++ 02-service-stack-shared/src/main/java/eu/isygoit/mapper/EntityMinFullMapper.java
@@ -11,7 +11,7 @@ import java.util.Set;
  * @param <MinD> the type parameter
  * @param <FullD> the type parameter
  */
-public interface EntityMinFullMapper<T, MinD, FullD extends MinD> {
+public interface EntityMinFullMapper<T, MinD, FullD extends MinD> extends EntityMapper<T, FullD>{
 
     /**
      * Dto to entity t.
@@ -60,52 +60,4 @@ public interface EntityMinFullMapper<T, MinD, FullD extends MinD> {
      * @return the set
      */
     Set<MinD> listEntityToDtoMin(Set<T> list);
-
-    /**
-     * Dto to entity t.
-     *
-     * @param object the object
-     * @return the t
-     */
-    T dtoToEntity(FullD object);
-
-    /**
-     * Entity to dto d.
-     *
-     * @param object the object
-     * @return the d
-     */
-    FullD entityToDto(T object);
-
-    /**
-     * List dto to entity list.
-     *
-     * @param list the list
-     * @return the list
-     */
-    List<T> listDtoToEntity(List<FullD> list);
-
-    /**
-     * List dto to entity set.
-     *
-     * @param list the list
-     * @return the set
-     */
-    Set<T> listDtoToEntity(Set<FullD> list);
-
-    /**
-     * List entity to dto list.
-     *
-     * @param list the list
-     * @return the list
-     */
-    List<FullD> listEntityToDto(List<T> list);
-
-    /**
-     * List entity to dto set.
-     *
-     * @param list the list
-     * @return the set
-     */
-    Set<FullD> listEntityToDto(Set<T> list);
 }
